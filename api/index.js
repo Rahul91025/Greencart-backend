a42087c996e7807040e0999b5c6efa1293026747
Rahul Gupta <rahulguptacsit@gmail.com>
2025-09-21 23:47:42 +0530

    changes

--- api/index.js
+++
@@ -1,2 +0,0 @@
-const app = require("../src/server.js");
-module.exports = (req, res) => app(req, res);
--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use("/api/simulations", simulationRoutes);
 
 app.use(errorHandler);
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 
 
 
